94d242d6359e5ab89cf1cca4ed4ba215ecbcddde
Nicolas Sentout <nicolas.sentout@hotmail.com>
2019-11-18 20:25:56 +0100

    fix adj matrix

--- exact.py
+++ exact.py
@@ -66,14 +66,13 @@ class Exact :
                             scoring_kicks.append([opponent, direction + math.pi])
                 direction += self.problem.theta_step
 
-        possible_defs = []
         for x in self.frange(self.problem.field_limits[0][0], self.problem.field_limits[0][1],
         self.problem.pos_step):
             for y in self.frange(self.problem.field_limits[1][0], self.problem.field_limits[1][1],
             self.problem.pos_step):
                 defense = [x, y]
                 adj_line = [0] * len(scoring_kicks)
-                if self.dist(possible_defs, defense, self.problem.robot_radius):
+                if self.dist(self.possible_defs, defense, self.problem.robot_radius):
                     if self.dist(self.opponents_pos, defense, self.problem.robot_radius):
                         for kick in scoring_kicks:
                             if not (segmentCircleIntersection(kick[0],
@@ -91,60 +90,6 @@ class Exact :
                             else:
                                 self.coord_map[str(adj_line)] = [[x,y]]
         
-        print("matrix's size = " + str(len(self.adj_mat)) + ", " +
-        str(len(self.adj_mat[0])))
-        
-    def buildAdjacencyMatrix2(self):
-        for i in range(len(self.problem.opponents[0])):
-            self.opponents_pos.append([self.problem.opponents[0][i], self.problem.opponents[1][i]])
-
-        posts = []
-        for i in range(len(self.problem.goals)):
-            posts.append([self.problem.goals[i].posts[0][0],
-            self.problem.goals[i].posts[1][0]])
-            posts.append([self.problem.goals[i].posts[0][1],
-            self.problem.goals[i].posts[1][1]])
-
-        scoring_kicks = []
-        for i in range(len(self.problem.opponents[0])):
-            opponent = [self.problem.opponents[0][i], self.problem.opponents[1][i]]
-            direction = 0
-            while direction < 2 * math.pi:
-                for goal in self.problem.goals:
-                    if not (goal.kickResult(opponent, direction) is None):
-                        """ 4 next lines to correct incomprehensible kickResult()'s
-                            behavior """
-                        if (direction > math.pi):
-                            scoring_kicks.append([opponent, direction - math.pi])
-                        elif (direction < math.pi):
-                            scoring_kicks.append([opponent, direction + math.pi])
-                direction += self.problem.theta_step
-
-        possible_defs = []
-        for x in self.frange(self.problem.field_limits[0][0], self.problem.field_limits[0][1],
-        self.problem.pos_step):
-            for y in self.frange(self.problem.field_limits[1][0], self.problem.field_limits[1][1],
-            self.problem.pos_step):
-                defense = [x, y]
-                adj_line = [0] * len(scoring_kicks)
-                if defense not in possible_defs:
-                    if defense not in posts:
-                        if self.dist(self.opponents_pos, defense, self.problem.robot_radius):
-                            for kick in scoring_kicks:
-                                if not (segmentCircleIntersection(kick[0],
-                                self.anotherPoint(kick[0], kick[1]), defense, self.problem.robot_radius)
-                                is None):
-                                    adj_line[scoring_kicks.index(kick)] = 1
-                                    possible_defs.append(defense)
-                            if 1 in adj_line:
-                                self.adj_mat.append(adj_line)
-
-                                if (str(adj_line) in self.coord_map):
-                                    self.coord_map[str(adj_line)].append([x, y])
-                                            
-                                else:
-                                    self.coord_map[str(adj_line)] = [[x,y]]
-            
         print("matrix's size = " + str(len(self.adj_mat)) + ", " +
         str(len(self.adj_mat[0])))
 
